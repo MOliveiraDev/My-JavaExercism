4755ac7c6d25a84a863ed516b288b8bf9f9a619f
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-02 16:20:55 -0300

    Update systemtimer.java

--- Lang/src/systemtimer.java
+++ Lang/src/systemtimer.java
@@ -1,16 +1,18 @@
-import java.util.Timer;
+import java.util.Date;
 
 public class systemtimer {
     public static void main(String[] args) {
-        Timer relogio = new Timer();
-        printartempo();
-        System.out.println(relogio);
+        
         
     }
 
-    static void printartempo()
+    static void timer()
     {
-        System.out.println("Este código diz a hora, o mês, a semana, o ano em quem nos estamos");
-        
+        Date relogio = new Date();
+        System.out.println("Esse ");
+        System.out.println(relogio.toString());
+
     }
 }
+
+
