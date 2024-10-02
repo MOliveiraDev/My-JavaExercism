4474e438ea9f2088e0dd7d0dc82adc47aefa48f0
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-02 16:08:29 -0300

    Update lang.java

--- Lang/src/lang.java
+++ Lang/src/lang.java
@@ -4,6 +4,6 @@ public class lang {
     public static void main(String[] args) {
         //Java consegue detectar a lingua que eu estou usando no momento
         Locale locale = new Locale("PORTUGUESE", "PT");
-        System.out.println("Linguagem: " + locale.getDisplayLanguage());
+        System.out.println("Lingua do sistema operacional: " + locale.getDisplayLanguage());
     }
 }
