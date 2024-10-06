338a6835b2a0666f79e6dac2951e657bb8cfe7df
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-05 22:22:59 -0300

    Update typesprimite.java

--- Lang/src/typesprimite.java
+++ Lang/src/typesprimite.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class typesprimite {
 
     public static void main(String[] args) {
-        String nome = "Adriel tirou ";
+        Scanner digitar = new Scanner(System.in);
+        String nome = "\nAdriel tirou ";
         var nota = 2.1;
         String frase2 = " em matemática";
         System.out.println(nome + nota + frase2);
+        System.out.println("qual é a sua opinião ?");
+        String opiniao = digitar.nextLine(); 
+
+        if(opiniao.equalsIgnoreCase("burro demais")) {
+            System.out.println("\nConcordo plenamente");
+        }
+
+        digitar.close();
+        
     }
 }
