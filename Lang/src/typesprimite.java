8c3492c85c809e641937904af79d9343bc58215e
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-05 21:59:05 -0300

    Update typesprimite.java

--- Lang/src/typesprimite.java
+++ Lang/src/typesprimite.java
@@ -1,8 +1,9 @@
 public class typesprimite {
 
     public static void main(String[] args) {
-        String nome = "Adriel";
+        String nome = "Adriel tirou ";
         var nota = 2.1;
-        
+        String frase2 = " em matemática";
+        System.out.println(nome + nota + frase2);
     }
 }
