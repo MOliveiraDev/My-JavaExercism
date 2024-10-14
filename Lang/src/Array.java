78e7635164725993b599243e733632ce6a861f81
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-14 20:05:42 -0300

    a

--- Lang/src/Array.java
+++ Lang/src/Array.java
@@ -1,9 +1,13 @@
 public class Array {
     public static void main(String[] args) {
+
         int [] numerosinteiros = {1,2,3,4,5};
+        String [] nomes = {"Fernando", "Julia", "Adriel", ""};
+
         System.out.println("\nO numero que aparecer vai está dentro dos numeros inteiros\n");
 
         System.out.println(numerosinteiros[0]);
+        System.out.println(nomes[0]);
 
     }
 }
