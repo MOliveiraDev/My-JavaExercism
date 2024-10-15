d7e53f4d0778db612eb16a9db73e78501944e76f
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-15 02:29:03 -0300

    Delta

--- Lang/src/Array.java
+++ Lang/src/Array.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
 
-        int [] numerosinteiros = {1,2,3,4,5};
+        int[] numerosinteiros = {1, 2, 3, 4, 5};
 
-        String [] nomes = {"Fernando", "Julia", "Adriel", "Carlinhos", "Caue"};
+        String[] nomes = {"Fernando", "Julia", "Adriel", "Carlinhos", "Caue"};
 
         System.out.println("\nO numero que aparecer vai está dentro dos numeros inteiros\n");
 
@@ -12,5 +14,31 @@ public class Array {
         System.out.println("O nome que aparecer vai está dentro dos nomes\n");
         System.out.println(nomes[0]);
         System.out.println("-------------------------\n");
+        System.out.println("");
+        Alfabeto();
+
+    }
+
+    static void Alfabeto() {
+
+        String [] letras = {"A", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z"}; //Array de letras
+
+        Scanner digitar = new Scanner(System.in); //momento de digitar
+
+        System.out.println("digite um numero que está no alfabeto");
+
+        int numero = digitar.nextInt(); /*agora a variável "digitar" vai ter
+                                        o valor que eu adicionei ao inteiro "numero"*/
+
+
+        System.out.println(letras[numero]);
+
+        /*O sistema vai mostrar a posição que eu escolhi do alfabeto
+        e de acordo com essa posição, vai me retorna uma palavra */
+
+
+
+
     }
 }
---
+++ Lang/src/deltaprint.java
@@ -0,0 +1,15 @@
+public class deltaprint {
+    public static void main(String[] args) {
+
+        String l1 = "*";
+        String l2 = "***";
+        String l3 = "*****";
+        String l4 = "*******";
+        String l5 = "*********";
+
+        System.out.println("\n" + l1 + "\n" + l2 + "\n" + l3 + "\n" + l4 + "\n" + l5 + "\n");
+
+        System.out.println("Esse é triangulo retângulo na programação");
+
+    }
+}
