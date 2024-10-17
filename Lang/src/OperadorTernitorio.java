4a3585b07d803c40d33585d3fea2f3bea0b6cdc9
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-16 21:18:20 -0300

    Ternitório

---
+++ Lang/src/OperadorTernitorio.java
@@ -0,0 +1,34 @@
+public class OperadorTernitorio {
+    public static void main(String[] args) {
+        Operador1();
+        Operador2();
+    }
+
+    static void Operador1(){
+
+        int n1, n2 ,r;
+        n1 = 4;
+        n2 = 10;
+        r = (n1>n2) ? 0 : 1;
+        System.out.println("");
+        System.out.println(r);
+
+    }
+
+    static void Operador2(){
+
+        String nome1 = "Maicon";
+        String nome2 = "Maicon";
+        String nome3 = new String("Maicon");
+        String nome4 = new String("Maicon");
+        String r;
+        r = (nome1.equals(nome3)) ? "É Igual" : "É diferente";
+        System.out.println("");
+        System.out.println(r);
+
+    }
+
+    static void LogicOperator(){
+
+    }
+}
