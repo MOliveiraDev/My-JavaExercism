f8ec3d75883cc87bb55507d5628d813dd41f57ef
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-01-06 19:50:57 -0300

    a

--- Lang/src/Conditions.java
+++ Lang/src/Conditions.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Conditions {
     public static void main(String[] args) {
+
         Scanner digitar = new Scanner(System.in);
-
+        System.out.println("Nota Parcial do Aluno: ");
         float n1 = digitar.nextFloat();
+        System.out.println("Nota Bimestral do Aluno: ");
         float n2 = digitar.nextFloat();
-        float media = (n1 + n2)/2;
+        System.out.println("Sua média: ");
+        float media = (n1 + n2) / 2;
+        System.out.println(media+"\n");
+        String resultado = (media > 5) ? "Sua nota está na média!" : "Precisa melhora a sua nota";   //como substituir o if e o else
 
-        System.out.println(media);
-        if(media>9) {
-            System.out.println("você é fera");
-        }
-
+        System.out.println(resultado);
 
     }
 }
